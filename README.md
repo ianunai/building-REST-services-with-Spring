872131e3dc9cb443540f0490409522f7d7cd452c
Anunai Ishan <ishananunai@gmail.com>
2021-01-02 23:08:02 -0500

    Implement REST API with support and HATEOAS

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Tutorial on building REST services with Spring found [here](https://spring.io/gu
 
 ## Getting Started
 
-[Spring Initialzr](https://start.spring.io/) can be used to create a Spring project.
+[Spring Initializr](https://start.spring.io/) can be used to create a Spring project.
+
+## Using HATEOAS
+
+Spring provides support for following HATEOAS principles through the [Spring HATEOAS library](https://spring.io/projects/spring-hateoas).
--- src/main/java/payroll/Employee.java
+++ src/main/java/payroll/Employee.java
@@ -10,13 +10,15 @@ import javax.persistence.Id;
 class Employee {
 
     private @Id @GeneratedValue Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String role;
 
     public Employee() {}
 
-    public Employee(String name, String role) {
-        this.name = name;
+    public Employee(String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
     }
 
@@ -29,11 +31,13 @@ class Employee {
     }
 
     public String getName() {
-        return this.name;
+        return this.firstName + " " + this.lastName;
     }
 
     public void setName(String name) {
-        this.name = name;
+        String[] parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
     }
 
     public String getRole() {
@@ -42,6 +46,22 @@ class Employee {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+         return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     @Override
@@ -55,18 +75,20 @@ class Employee {
 
         Employee employee = (Employee) obj;
         return Objects.equals(this.id, employee.id) &&
-                Objects.equals(this.name, employee.name) &&
+                Objects.equals(this.firstName, employee.firstName) &&
+                Objects.equals(this.lastName, employee.lastName) &&
                 Objects.equals(this.role, employee.role);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id, this.name, this.role);
+        return Objects.hash(this.id, this.firstName, this.lastName, this.role);
     }
 
     @Override
     public String toString() {
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+        return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName
+                + '\'' + ", role='" + this.role + '\'' + '}';
     }
 
 }
